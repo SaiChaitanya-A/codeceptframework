0ed3c016acc16b8a2e10938a507ec589c9a4658c
SaiChaitanya-A <chaitanya.avanigadda@excellarate.com>
2022-10-14 13:27:43 +0530

    adding

--- tests/test.js
+++ tests/test.js
@@ -9,7 +9,15 @@ Scenario("flight search", async ({ I }) => {
     //I.click('//input[@id="ctl00_mainContent_ddl_destinationStation1_CTXT"]');
     I.fillField('//input[@id="ctl00_mainContent_ddl_destinationStation1_CTXT"]', 'Hyderabad (HYD)');
     I.seeElement('//div[@id="ui-datepicker-div"]');
-    I.click(`//a[@class="ui-state-default ui-state-active" and text()="18"]`);
+    I.click(`//td[@data-month="9"]//a[text()="18"]`);
+    //I.click("//input[@name='ctl00$mainContent$view_date2']");
+    I.click("//label[contains(text(),'Return date')]/../..//input[@name='ctl00$mainContent$view_date2']");
+    I.click(`//td[@data-month="9"]//a[text()="27"]`);
+    I.see('1 Adult');
+    I.selectOption('//label[contains(.,"Currency")]/../..//select', 'AED');
+
+    ////label[text()='Round Trip']/../..//input[@id="ctl00_mainContent_rbtnl_Trip_1"];
+
 
     I.wait(5);
 
